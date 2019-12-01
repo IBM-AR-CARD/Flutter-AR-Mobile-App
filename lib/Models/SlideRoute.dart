31c9ac19f78b337e6cba6a6ed793b64b36e27fc9
KZHIWEI <zcabhaf@ucl.ac.uk>
2019-12-01 01:00:29 +0000

    add QR scan function

--- lib/Models/SlideRoute.dart
+++ lib/Models/SlideRoute.dart
@@ -35,8 +35,7 @@ class SlideLeftRoute extends PageRouteBuilder {
         BuildContext context,
         Animation<double> animation,
         Animation<double> secondaryAnimation,
-        ) =>
-    page,
+        ) => page,
     transitionsBuilder: (
         BuildContext context,
         Animation<double> animation,
@@ -46,6 +45,88 @@ class SlideLeftRoute extends PageRouteBuilder {
         SlideTransition(
           position: Tween<Offset>(
             begin: const Offset(1, 0),
+            end: Offset.zero,
+          ).animate(new CurvedAnimation(
+              parent: animation,
+              curve: Curves.easeInOut
+          )),
+          child: child,
+        ),
+  );
+}
+class FadeRoute extends PageRouteBuilder {
+  final Widget page;
+  FadeRoute({this.page})
+      : super(
+    pageBuilder: (
+        BuildContext context,
+        Animation<double> animation,
+        Animation<double> secondaryAnimation,
+        ) =>
+    page,
+    transitionsBuilder: (
+        BuildContext context,
+        Animation<double> animation,
+        Animation<double> secondaryAnimation,
+        Widget child,
+        ) =>
+        FadeTransition(
+          opacity: animation,
+          child: child,
+        ),
+  );
+}
+
+class ScaleRotateRoute extends PageRouteBuilder {
+  final Widget page;
+  ScaleRotateRoute({this.page})
+      : super(
+    pageBuilder: (
+        BuildContext context,
+        Animation<double> animation,
+        Animation<double> secondaryAnimation,
+        ) =>
+    page,
+    transitionDuration: Duration(seconds: 1),
+    transitionsBuilder: (
+        BuildContext context,
+        Animation<double> animation,
+        Animation<double> secondaryAnimation,
+        Widget child,
+        ) =>
+        ScaleTransition(
+          scale: Tween<double>(
+            begin: 0.0,
+            end: 1.0,
+          ).animate(
+            CurvedAnimation(
+              parent: animation,
+              curve: Curves.easeInOut,
+            ),
+          ),
+          child: child
+        ),
+  );
+}
+
+class SlideTopRoute extends PageRouteBuilder {
+  final Widget page;
+  SlideTopRoute({this.page})
+      : super(
+    pageBuilder: (
+        BuildContext context,
+        Animation<double> animation,
+        Animation<double> secondaryAnimation,
+        ) => page,
+    transitionsBuilder: (
+        BuildContext context,
+        Animation<double> animation,
+        Animation<double> secondaryAnimation,
+        Widget child,
+        ) =>
+        SlideTransition(
+          position: Tween<Offset>(
+            begin: const Offset(0, -1),
             end: Offset.zero,
           ).animate(new CurvedAnimation(
               parent: animation,
