91674b9cb609dae3eeabc9f1f36671dbfe820f38
KZHIWEI <zcabhaf@ucl.ac.uk>
2020-01-07 21:44:12 +0000

    change package of stt

--- lib/main.dart
+++ lib/main.dart
@@ -6,14 +6,15 @@ import 'Models/SlideRoute.dart';
 import 'Screens/Settings.dart';
 import 'package:flutter/services.dart';
 import 'package:flutter_unity_widget/flutter_unity_widget.dart';
-import 'package:speech_recognition/speech_recognition.dart';
 import 'dart:async';
 import 'package:flutter_tts/flutter_tts.dart';
+import 'package:speech_to_text/speech_to_text.dart';
+import 'package:speech_to_text/speech_recognition_result.dart';
+import 'package:speech_to_text/speech_recognition_error.dart';
+
 void main() {
-  SystemChrome.setPreferredOrientations([
-    DeviceOrientation.portraitUp,
-    DeviceOrientation.portraitDown
-  ]);
+  SystemChrome.setPreferredOrientations(
+      [DeviceOrientation.portraitUp, DeviceOrientation.portraitDown]);
   runApp(new MyApp());
 }
 
@@ -22,12 +23,11 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-        title: 'Flutter Demo',
-        theme: ThemeData(
+      title: 'Flutter Demo',
+      theme: ThemeData(
         primarySwatch: Colors.blue,
       ),
       home: MyHomePage(title: 'Flutter Demo Home Page'),
-
     );
   }
 }
@@ -42,55 +42,82 @@ class MyHomePage extends StatefulWidget {
 }
 
 class _MyHomePageState extends State<MyHomePage> {
-  bool _isAvailable = false;
-  bool _isListening = false;
-  String resultText = "";
   Timer _timer;
-  SpeechRecognition _speechRecognition;
   int _currentColor = 1;
   int _start = 0;
   String _QRText = 'QR';
   UnityWidgetController _unityWidgetController;
-  List<Color> _colors = [ //Get list of colors
-    Color.fromARGB(255,112,112,112),
-    Color.fromARGB(255,15,232,149),
+  List<Color> _colors = [
+    //Get list of colors
+    Color.fromARGB(255, 112, 112, 112),
+    Color.fromARGB(255, 15, 232, 149),
   ];
+  bool _hasSpeech = false;
+  String lastWords = "";
+  String lastStatus = "";
+  final SpeechToText speech = SpeechToText();
   static final int age = 21;
   static final String name = "henry";
   static final String description = "second year university student";
   final FlutterTts flutterTts = FlutterTts();
 
   @override
-  void initState(){
+  void initState() {
     super.initState();
     SystemChrome.setEnabledSystemUIOverlays([]);
-    initSpeechRecognizer();
     flutterTts.setLanguage("en-US");
+    initSpeechState();
   }
 
-  speak (String text) async{
+  Future<void> initSpeechState() async {
+    bool hasSpeech = await speech.initialize(onStatus: statusListener);
+
+    if (!mounted) return;
+    setState(() {
+      _hasSpeech = hasSpeech;
+    });
+  }
+  talk(String text)async {
+    setMessage('changeAnimator', "talking");
+    await flutterTts.speak("text");
+    setMessage('changeAnimator', "idle");
+  }
+  speak(String text) async {
     text = text.toLowerCase();
-    if(text.contains("age")){
-      await flutterTts.speak("I am $age years old");
-    }else if(text.contains("name")){
-      await flutterTts.speak("my name is $name");
-    }else if(text.contains("description")){
-      await flutterTts.speak("$description");
+    if (text.contains("age")) {
+      await talk("I am $age years old");
+    } else if (text.contains("name")) {
+      await talk("my name is $name");
+    } else if (text.contains("description")) {
+      await talk("$description");
+    }
+    if(lastWords=="start talking"){
+      setMessage('changeAnimator', "talking");
+    }
+    if(lastWords=="stop"){
+      setMessage('changeAnimator', "idle");
+    }
+    if(lastWords=="start dancing"){
+      setMessage('changeAnimator', "dancing");
+    }
+    if(lastWords=="random character"){
+      setMessage('randomModel', '');
     }
   }
+
   void startTimer() {
     const oneSec = const Duration(seconds: 1);
     _timer = new Timer.periodic(
       oneSec,
-          (Timer timer) => setState(
-            () {
+      (Timer timer) => setState(
+        () {
           if (_start > 30) {
-            recordEnd();
+            stopListening();
             _timer.cancel();
             _start = 0;
-          } else if(_isListening) {
+          } else if (speech.isListening) {
             _start = _start + 1;
-          }else{
+          } else {
             _start = 0;
           }
         },
@@ -98,51 +125,48 @@ class _MyHomePageState extends State<MyHomePage> {
     );
   }
 
-  void initSpeechRecognizer(){
-    _speechRecognition = SpeechRecognition();
-    _speechRecognition.setAvailabilityHandler(
-            (bool result ) => setState(()=> _isAvailable=result)
-    );
-    _speechRecognition.setRecognitionStartedHandler(()=>setState(
-            ()=>_isListening=true)
-    );
-    _speechRecognition.setRecognitionResultHandler(
-            (String speech)=>setState((){
-              resultText=speech;
-              speak(resultText);
-              if(resultText=="start talking"){
-                setMessage('changeAnimator', "talking");
-              }
-              if(resultText=="stop"){
-                setMessage('changeAnimator', "idle");
-              }
-              if(resultText=="start dancing"){
-                setMessage('changeAnimator', "dancing");
-              }
-              if(resultText=="random character"){
-                setMessage('randomModel', '');
-              }
-            })
-    );
-
-    _speechRecognition.setRecognitionCompleteHandler(()=>setState(
-            ()=>_isListening=false)
-    );
-    _speechRecognition.activate().then(
-            (result) => setState(()=>_isAvailable=result)
-    );
+  void startListening() {
+    startTimer();
+    lastWords = "";
+    speech.listen(onResult: resultListener);
+    setState(() {});
   }
+
+  void stopListening() {
+    speech.stop();
+    setState(() {
+    });
+  }
+
+  void cancelListening(ctx ) async{
+    speech.cancel();
+    setState(() {
+      speak(lastWords);
+    });
+  }
+
+  void resultListener(SpeechRecognitionResult result) {
+    setState(() {
+      lastWords = "${result.recognizedWords}";
+    });
+  }
+
+  void statusListener(String status) {
+    setState(() {
+      lastStatus = "$status";
+    });
+  }
+
 
   void navigateToScan() async {
     setMessage('changeText', "opened scan");
     _unityWidgetController.pause();
     final result = await Navigator.push(
       context,
-      SlideTopRoute(page:ScanQR()),
+      SlideTopRoute(page: ScanQR()),
     );
     setState(() {
       _QRText = result == '' ? 'QR' : result;
-
     });
     await _unityWidgetController.resume();
     setMessage('changeText', _QRText);
@@ -151,17 +175,18 @@ class _MyHomePageState extends State<MyHomePage> {
 //      _currentColor = _currentColor ^ 1;
 //    });
   }
-  Widget bottomRow(){
-        return new Padding(
-          padding: const EdgeInsets.all(50.0),
-          child:Row(
-              mainAxisAlignment: MainAxisAlignment.spaceBetween,
-            children:<Widget>[
+
+  Widget bottomRow() {
+    return new Padding(
+        padding: const EdgeInsets.all(50.0),
+        child: Row(
+            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+            children: <Widget>[
               IconButton(
-                onPressed: (){
+                onPressed: () {
                   Navigator.push(
                     context,
-                    SlideRightRoute(page:MyCards()),
+                    SlideRightRoute(page: MyCards()),
                   );
                 },
                 tooltip: 'List',
@@ -172,20 +197,18 @@ class _MyHomePageState extends State<MyHomePage> {
                 ),
               ),
               GestureDetector(
-                onTap: _isListening ? recordEnd : recordStart,
-                onPanEnd: recordCancel,
-                child: Icon(
-                  _isListening ? Icons.stop : Icons.mic,
-                  color: Colors.white,
-                  size: 60,
-                )
-
-              ),
+                  onTap: speech.isListening ? stopListening : startListening,
+                  onPanEnd: cancelListening,
+                  child: Icon(
+                    speech.isListening ? Icons.stop : Icons.mic,
+                    color: Colors.white,
+                    size: 60,
+                  )),
               IconButton(
-                onPressed: (){
+                onPressed: () {
                   Navigator.push(
                     context,
-                    SlideLeftRoute(page:Settings()),
+                    SlideLeftRoute(page: Settings()),
                   );
                 },
                 tooltip: 'Person',
@@ -195,93 +218,59 @@ class _MyHomePageState extends State<MyHomePage> {
                   color: Colors.white,
                 ),
               ),
-          ]
-          )
-        );
+            ]));
   }
-  void recordStart(){
-    if(_isAvailable && !_isListening){
-      _start = 0;
-      startTimer();
-      print("start record");
-      _speechRecognition.listen(locale: "en_GB").then(
-          (result) => print('record : $result')
-      );
-    }
-  }
-  void recordEnd(){
-    print("_isAvailable : $_isAvailable");
-    if(_isListening && _start>5) {
-      _speechRecognition.stop().then(
-              (result) => setState(() {
-                _start = 0;
-                _isListening = result;
-              }));
-    }
-  }
-  void recordCancel(ctx){
-      if(_isListening)
-        _speechRecognition.cancel().then((result)=>setState((){
 
-          _isListening = result;
-          resultText = "";
-        }));
-  }
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Container(
-        child: Stack(
-          children: <Widget>[
-            Opacity(
-              opacity: _currentColor==1? 1 : 0,
-              child: UnityWidget(
-                onUnityViewCreated: onUnityCreated,
-                isARScene: true,
-                onUnityMessage: onUnityMessage
-              )
-            ),
-            Center(
-              child: Opacity(
-                opacity: 0.5,
-                child: Container(
-                  width: MediaQuery.of(context).size.width*0.6,
-                  height: 40,
-                  decoration: BoxDecoration(
-                    color:Colors.white,
+        body: Container(
+          child: Stack(
+            children: <Widget>[
+              Opacity(
+                  opacity: _currentColor == 1 ? 1 : 0,
+                  child: UnityWidget(
+                      onUnityViewCreated: onUnityCreated,
+                      isARScene: true,
+                      onUnityMessage: onUnityMessage)),
+              Center(
+                child: Opacity(
+                  opacity: 0.5,
+                  child: Container(
+                    width: MediaQuery.of(context).size.width * 0.6,
+                    height: 40,
+                    decoration: BoxDecoration(
+                      color: Colors.white,
+                    ),
+                    child: Text('$lastWords : $_start'),
                   ),
-                  child:Text('$resultText : $_start'),
+                ),
               ),
-            ),
-            ),
-            Padding(
-              padding: EdgeInsets.only(top:70.0,left: 20.0),
-              child: FlatButton(
-                child: new  Text(
+              Padding(
+                padding: EdgeInsets.only(top: 70.0, left: 20.0),
+                child: FlatButton(
+                  child: new Text(
                     _QRText,
                     textAlign: TextAlign.left,
                     style: TextStyle(
                       fontSize: 40.0,
                       color: _colors[_currentColor],
                     ),
+                  ),
+                  onPressed: navigateToScan,
+                  splashColor: Colors.transparent,
+                  highlightColor: Colors.transparent,
                 ),
-                onPressed: navigateToScan,
-                splashColor: Colors.transparent,
-                highlightColor: Colors.transparent,
               ),
-            ),
-          ],
+            ],
+          ),
         ),
-
-      ),
         floatingActionButtonLocation: FloatingActionButtonLocation.centerDocked,
-        floatingActionButton:bottomRow()
-
-    );
+        floatingActionButton: bottomRow());
   }
 
   void setMessage(String function, String msg) {
-    print("Sending message to unity: "+msg);
+    print("Sending message to unity: " + msg);
     _unityWidgetController.postMessage(
       'Main Camera',
       function,
@@ -294,13 +283,13 @@ class _MyHomePageState extends State<MyHomePage> {
     setState(() {
       _QRText = message.toString();
     });
-
   }
 
-  // Callback that connects the created controller to the unity controller
+// Callback that connects the created controller to the unity controller
   void onUnityCreated(controller) {
     this._unityWidgetController = controller;
   }
+
   @override
   void dispose() {
     _timer.cancel();
